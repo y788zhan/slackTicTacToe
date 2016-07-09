3d6bd2c449e4a1703fc0bdef3bf10047587e8923
Richard Zhang <richardzhang@Richards-MacBook-Pro.local>
2016-07-09 17:51:33 -0400

    can view body now yay

--- index.js
+++ index.js
@@ -49,12 +49,22 @@ app.post('/usage', function(req, res) {
 	});
 });
 
+  1   2   3
+1 o | x | x
+ ---+---+---
+2 o | x | x
+ ---+---+---
+3   |   |
+
 app.post('/start', function(req, res) {
 	res.status(200).json({
 	    "response_type": "in_channel",
-	    "text": "It's 80 degrees right now. BODY: " +
-	    req.body.token + " " + req.body.team_id //+ " " req.body.channel_id + 
-	    //" " + req.body.user_id + " " + req.body.user_name + " " + req.body.text
+	    "text": "  1   2   3\n" + 
+				"1 o | x | x\n" +
+ 				" ---+---+---\n" +
+				"2 o | x | x\n" +
+				" ---+---+---\n" +
+				"3   |   |   "
 	});
 });
 
