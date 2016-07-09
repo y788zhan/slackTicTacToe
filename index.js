a5ab7e9c873bfefa592283cfee4d5be416bb12ca
Richard Zhang <richardzhang@Richards-MacBook-Pro.local>
2016-07-09 17:31:20 -0400

    can view body now yay

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ app.post('/start', function(req, res) {
 	res.status(200).json({
 	    "response_type": "in_channel",
 	    "text": "It's 80 degrees right now. BODY: " +
-	    req.body.token + " " + req.body.team_id + " " req.body.channel_id + 
-	    " " + req.body.user_id + " " + req.body.user_name + " " + req.body.text
+	    req.body.token //+ " " + req.body.team_id + " " req.body.channel_id + 
+	    //" " + req.body.user_id + " " + req.body.user_name + " " + req.body.text
 	});
 });
 
