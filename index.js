a399cef34963b828f5ef69123f36576dad667fd3
Richard Zhang <richardzhang@Richards-MacBook-Pro.local>
2016-07-09 15:11:14 -0400

    defined all endpoints

--- index.js
+++ index.js
@@ -13,9 +13,35 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+
+// TIC TAC TOE API ENDPOINTS
+app.get('/usage', function(req, res) {
+	res.status(200).json({"message": "TODO usage details"});
+});
+
 app.post('/start', function(req, res) {
 	res.status(200).json({"message": "TODO start game"});
 });
+
+app.post('/quit', function(req, res) {
+	res.status(200).json({"message": "TODO quit game"});
+})
+
+app.post('/restart', function(req, res) {
+	res.status(200).json({"message": "TODO restart game"});
+})
+
+app.post('/move', function(req, res) {
+	res.status(200).json({"message": "TODO player move"});
+})
+
+app.get('/gamestate', function(req, res) {
+	res.status(200).json({"message": "TODO return game state"});
+});
+
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
