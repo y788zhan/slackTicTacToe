3bbd5c0c9e09f080007cc024653090fe3a186181
Richard Zhang <richardzhang@Richards-MacBook-Pro.local>
2016-07-09 15:03:35 -0400

    added endpoint

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/start', function(req, res) {
+	res.status(200).json({"message": "TODO start game"});
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
