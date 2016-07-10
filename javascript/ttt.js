162bb63f0b6cde0f0c446ace2777938063b75d24
Richard Zhang <richardzhang@Richards-MacBook-Pro.local>
2016-07-10 14:53:23 -0400

    str

--- javascript/ttt.js
+++ javascript/ttt.js
@@ -35,7 +35,7 @@ playersObj schema:
 
 
 String.prototype.replaceAt=function(index, character) {
-	console.log(this, this.length);
+	console.log(this, this.length, index, character);
 	console.log(this.substr(0, index), character, this.substr(index + 1, this.length));
     return this.substr(0, index) + character + this.substr(index + 1, this.length);
 }
